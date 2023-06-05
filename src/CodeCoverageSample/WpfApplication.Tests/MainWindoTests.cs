8f98a8c8fdbf9854957095277e2aa89274f894ac
fhnaseer <fhnaseer@live.com>
2023-06-05 16:44:16 +0200

    Adding wpf application

---
+++ src/CodeCoverageSample/WpfApplication.Tests/MainWindoTests.cs
@@ -0,0 +1,11 @@
+﻿namespace WpfApplication.Tests;
+
+[TestClass]
+public class MainWindoTests
+{
+    [TestMethod]
+    public void Add_Test()
+    {
+        Assert.AreEqual(5, MainWindow.Add(3, 2));
+    }
+}
---
+++ src/CodeCoverageSample/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace WpfApplication;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Result.Text = Add(int.Parse(One.Text), int.Parse(Two.Text)).ToString();
+    }
+}
