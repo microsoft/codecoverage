489a4258da12b26ee35f2dc15a81d08fe5f1dc1b
Jakub Chocholowicz <jachocho@microsoft.com>
2023-08-21 13:55:10 +0200

    More

---
+++ docs/examples/LibraryTests/library/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace library;
+
+public static class Class1
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    public static int Multiple(int x, int y)
+    {
+        return x * y;
+    }
+}
