eeddbd807b048e402149f222f2e6b82b771c7c47
Jakub Chocholowicz <jachocho@microsoft.com>
2023-08-22 16:15:14 +0200

    Add console.test

---
+++ samples/Calculator/tests/Calculator.Console.Tests/ConsoleTests.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using System.IO;
+
+namespace Calculator.Console.Tests;
+
+public class ConsoleTests
+{
+#if DEBUG
+    private const string Configuration = "Debug";
+#else
+    private const string Configuration = "Release";
+#endif
+
+    [Fact]
+    public void AddTest()
+    {
+        Assert.Equal(7, Execute("add 3 4"));
+    }
+
+    [Fact]
+    public void MultiplyTest()
+    {
+        Assert.Equal(12, Execute("multiply 3 4"));
+    }
+
+    [Fact]
+    public void SubstractTest()
+    {
+        Assert.Equal(1, Execute("substract 5 4"));
+    }
+
+    [Fact]
+    public void DevideTest()
+    {
+        Assert.Equal(4, Execute("devide 12 3"));
+    }
+
+    private double Execute(string arguments)
+    {
+        Process process = new Process();
+        process.StartInfo = new ProcessStartInfo()
+        {
+            FileName = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "..", "src", "Calculator.Console", "bin", Configuration, "net7.0", "Calculator.Console.exe"),
+            Arguments = arguments,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true
+        };
+        process.Start();
+        double result = double.Parse(process.StandardOutput.ReadToEnd());
+        process.WaitForExit();
+        return result;
+    }
+}
