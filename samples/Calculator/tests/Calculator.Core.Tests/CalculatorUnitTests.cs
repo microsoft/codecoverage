a3f8c30bd018ae41616e4b4fe7764144cf1ac43e
Jakub Chocholowicz <jachocho@microsoft.com>
2023-08-22 12:37:44 +0200

    First test project

---
+++ samples/Calculator/tests/Calculator.Core.Tests/CalculatorUnitTests.cs
@@ -0,0 +1,32 @@
+using Calculator.Core;
+using Xunit;
+
+namespace Calculator.Core.Tests;
+
+public class CalculatorUnitTests
+{
+    [Fact]
+    public void TestAdd()
+    {
+        Assert.Equal(11, OperationCalculator.CalculateAdd(2, 9));
+    }
+
+    [Fact]
+    public void TestMultiply()
+    {
+        Assert.Equal(18, OperationCalculator.CalculateMultiply(2, 9));
+    }
+
+    [Fact]
+    public void TestSubstract()
+    {
+        Assert.Equal(-7, OperationCalculator.CalculateSubstract(2, 9));
+    }
+
+
+    [Fact]
+    public void TestDevide()
+    {
+        Assert.Equal(9.0, OperationCalculator.CalculateDevide(18.0, 2.0));
+    }
+}
