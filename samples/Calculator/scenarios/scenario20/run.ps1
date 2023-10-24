6c3c5fa8f831d13a172191c293745e17b8a94925
Jakub Chocholowicz <jachocho@microsoft.com>
2023-10-24 10:13:51 +0200

    More

---
+++ samples/Calculator/scenarios/scenario20/run.ps1
@@ -0,0 +1,9 @@
+cd $PSScriptRoot/../..
+dotnet build
+dotnet tool install -g dotnet-coverage
+cd $PSScriptRoot/../../src/Calculator.Server
+dotnet-coverage collect --output report.coverage --session-id TagScenario19 "dotnet run --no-build" &
+cd $PSScriptRoot/../..
+dotnet test --settings ./scenarios/scenario19/coverage.runsettings --results-directory "$PSScriptRoot/../../TestResults/"
+dotnet-coverage shutdown TagScenario19
+dotnet-coverage merge -r --output-format cobertura --output merged.cobertura.xml $PSScriptRoot/../../TestResults/*.coverage $PSScriptRoot/../../src/Calculator.Server/report.coverage
