8e06efb3405e879d6ddb9bfc898bc183e686d9be
Jakub Chocholowicz <jachocho@microsoft.com>
2023-10-24 09:25:45 +0200

    Scenario 19 more

--- samples/Calculator/scenarios/scenario19/run.ps1
+++ samples/Calculator/scenarios/scenario19/run.ps1
@@ -2,8 +2,8 @@ cd $PSScriptRoot/../..
 dotnet build
 dotnet tool install -g dotnet-coverage
 cd $PSScriptRoot/../../src/Calculator.Server
-dotnet-coverage collect --output report.coverage --session-id TagScenario18 "dotnet run --no-build" &
+dotnet-coverage collect --output report.coverage --session-id TagScenario19 "dotnet run --no-build" &
 cd $PSScriptRoot/../..
-dotnet test --collect "Code Coverage" --results-directory "$PSScriptRoot/../../TestResults/"
-dotnet-coverage shutdown TagScenario18
+dotnet test --settings ./scenarios/scenario19/coverage.runsettings --results-directory "$PSScriptRoot/../../TestResults/"
+dotnet-coverage shutdown TagScenario19
 dotnet-coverage merge -r --output-format cobertura --output merged.cobertura.xml $PSScriptRoot/../../TestResults/*.coverage $PSScriptRoot/../../src/Calculator.Server/report.coverage
