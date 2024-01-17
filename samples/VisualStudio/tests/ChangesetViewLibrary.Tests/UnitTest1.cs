bcd461f6104a9079b366026ac31b0d4c40473b93
fhnaseer <fhnaseer@live.com>
2024-01-17 11:38:46 +0100

    adding changeset example,

---
+++ samples/VisualStudio/tests/ChangesetViewLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace ChangesetViewLibrary.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        _ = CoverageLibrary.ExistingMethodCovered();
+        _ = CoverageLibrary.ExistingMethod(1);
+        _ = CoverageLibrary.ExistingMethod(2);
+    }
+}
