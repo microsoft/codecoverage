170e4db016d5cea205519d9fe77356767a579b5b
Jakub Chocholowicz <jachocho@microsoft.com>
2024-02-01 09:58:00 +0100

    Native AOT example

---
+++ samples/Algorithms/src/Algorithms.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Algorithms.Core;
+
+int[] a = [1, 5, 10, 15];
+int[] b = [0, 3, 4, 6, 7, 20];
+
+Print(nameof(a), a);
+Print(nameof(b), b);
+Print("merged:", Merger.Merge(a, b));
+
+void Print(string name, int[] array)
+{
+    Console.WriteLine($"{name}: [{string.Join(",", array)}]");
+}
